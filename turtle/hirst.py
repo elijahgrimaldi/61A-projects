808278a826d6604068fedb247a8244d613ee85f7
elijahgrimaldi <114044477+elijahgrimaldi@users.noreply.github.com>
2023-05-10 22:06:10 -0700

    main

---
+++ turtle/hirst.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+import turtle as t
+import random
+import colorgram as color
+colors = color.extract('image.jpg', 6)
+timmy = Turtle()
+
+
+screen = Screen()
+screen.exitonclick
